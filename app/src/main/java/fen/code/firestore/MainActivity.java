1b182e46c75ca6beaf9a19fecff57b585a8d9e63
fentonmartin <fentonxm@gmail.com>
2019-01-27 15:33:30 +0700

    Initiate Project

---
+++ app/src/main/java/fen/code/firestore/MainActivity.java
@@ -0,0 +1,13 @@
+package fen.code.firestore;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
