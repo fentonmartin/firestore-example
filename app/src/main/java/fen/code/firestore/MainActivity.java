21c926d67b59616217046d54da1f1dffde45d4e6
fentonmartin <fentonxm@gmail.com>
2019-01-27 16:18:04 +0700

    Add Firebase Analytics instance

--- app/src/main/java/fen/code/firestore/MainActivity.java
+++ app/src/main/java/fen/code/firestore/MainActivity.java
@@ -3,11 +3,18 @@ package fen.code.firestore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.analytics.FirebaseAnalytics;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Firebase Analytics instance
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
     }
 }
